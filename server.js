5cbde07fa8c4cea0499db19805e9591c3fa6a35d
kong <k_sokhom@hotmail.fr>
2022-10-03 17:17:19 +0200

    nodemon/express installer

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require("http");
+const app = require("./app");
 
-const server = http.createServer((req, res) => {
-  res.end("Voila la réponse du new serveur !");
-});
+app.set("port", process.env.PORT || 3000);
+
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000);
