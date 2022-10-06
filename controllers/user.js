bdb124bac133baebd7d787e1ae93bafab9a80659
kong <k_sokhom@hotmail.fr>
2022-10-06 18:47:42 +0200

    routes d'authentification

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,22 @@
+const bcrypt = require("bcrypt");
+
+const User = require("../models/User");
+
+// cryptage du mot de passe
 exports.signup = (req, res, next) => {
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash, // création de nouveau user
+      });
+      user
+        .save() // enregistration dans la base de données
+        .then(() => res.status(201).json({ message: "Utilisateur créé ! " }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
 
-}
-
-exports.login = (req, res,  next) => {
-    
-}
+exports.login = (req, res, next) => {};
