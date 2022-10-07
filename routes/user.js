f47f5722bc9629ddc7669bbfb73859b573b6a872
kong <k_sokhom@hotmail.fr>
2022-10-07 18:30:42 +0200

    le serveur corrigé

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const userCtrl = requie("../controllers/user.js");
+const userCtrl = require("../controllers/user.js");
 
 router.post("/signup", userCtrl.signup);
 router.post("/login", userCtrl.login);
